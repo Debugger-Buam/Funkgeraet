bb0553709dcb8f48db1b144e88cbf7a43d5e8a04
Markus Perz <markus.perz@student.tugraz.at>
2020-11-06 09:58:14 +0100

    Add simple express server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from the Server!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
