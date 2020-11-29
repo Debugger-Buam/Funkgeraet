e4511ef43e48aa30270ae72cce290fe6418e06b0
Markus Perz <markus.perz@student.tugraz.at>
2020-11-29 19:42:17 +0100

    Improve error description

--- client/src/injection.ts
+++ client/src/injection.ts
@@ -13,19 +13,18 @@ export const Injectable = (): GenericClassDecorator<Type<unknown>> => {
 /**
  * The @type {DependencyContainer} class can be used for dependency
  * injection and inversion of control.
- * 
+ *
  * Bootstrapping creates instances of each class and it's dependencies
  * automagically.
  */
 export class DependencyContainer {
   private resolvedClasses = new Map();
 
-
   /**
    * This method allows for manual installation of a dependency.
    * This can be useful in cases where a dependency is required by
    * other classes can not be created automatically itself.
-   *  
+   *
    * @param target The type that should be resolvable
    * @param instance The concrete instance that will be resolved
    */
@@ -35,11 +34,11 @@ export class DependencyContainer {
 
   /**
    * Returns the resolved instance of a target type.
-   * 
+   *
    * @example
-   * 
+   *
    *  let service = get<IServiceName>(IServiceName);
-   * 
+   *
    * @param target The target type that should be resolved
    */
   public get<T>(target: Type<unknown>): T {
@@ -50,20 +49,25 @@ export class DependencyContainer {
    * Bootstraps all classes handed in to this method.
    * This guarantees the creation of each class and it's
    * dependencies.
-   * 
+   *
    * @param classes An array of the classes to create
    */
   public bootstrap(classes: Type<unknown>[]) {
     classes.forEach((c) => this.get<unknown>(c));
   }
 
-  private resolve<T>(target: Type<unknown>, dependencyTree: Type<unknown>[]): T {
+  private resolve<T>(
+    target: Type<unknown>,
+    dependencyTree: Type<unknown>[]
+  ): T {
     if (this.resolvedClasses.has(target)) {
       return this.resolvedClasses.get(target);
     }
 
     if (dependencyTree.includes(target)) {
-      throw new Error("=== CIRCULAR DEPENDENCY DETECTED ====");
+      throw new Error(
+        `DependencyContainer has detected a circular dependency with '${target}' class involved.`
+      );
     }
 
     dependencyTree.push(target);
